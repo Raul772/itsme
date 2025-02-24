bcbfc1e7288a7907b3cf52a5585399f513428054
Raul Victor <31050006+Raul772@users.noreply.github.com>
2025-02-24 01:18:37 -0300

    feat: add about section content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,3 +1,40 @@
+import box_circles from "../../../assets/box-circles.svg";
+import box_eyes from "../../../assets/box-eyes.svg";
+import stars_grouped from "../../../assets/Stars-grouped.svg";
+import styles from "./About.module.css";
+
 export default function About() {
-  return <div id="about">About</div>;
+  return (
+    <section id="about" className={styles.about}>
+      <div className={`container ${styles.aboutContainer}`}>
+        <div className={styles.aboutDecoration}>
+          <img src={box_eyes} alt="" />
+          <img src={stars_grouped} alt="" />
+        </div>
+
+        <div className={styles.aboutBox}>
+          <h2>Sobre mim</h2>
+          <p>
+            Sou graduando em fase final do curso de{" "}
+            <span className="highlight">Sistemas de Informação</span> com
+            interesse em{" "}
+            <span className="highlight">
+              desenvolvimento fullstack e DevOps.
+            </span>{" "}
+            Tive contato com React, JavaScript, TypeScript, Python e Express,
+            além de ORMs para bancos de dados. Tenho habilidades em comunicação
+            interpessoal, trabalho em equipe e{" "}
+            <span className="highlight">
+              estou constantemente em busca de desafios!
+            </span>
+          </p>
+        </div>
+
+        <div className={styles.aboutDecoration}>
+          <img src={stars_grouped} alt="" />
+          <img src={box_circles} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 }
