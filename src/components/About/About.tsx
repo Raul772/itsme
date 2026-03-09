9954838269d9fe00dd2e85926a9aae2d84489f82
raul772 <raulvictor772@gmail.com>
2026-03-09 09:13:11 -0300

    update: update About component text and change box shadow in AboutBox

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -15,14 +15,17 @@ export default function About() {
         <div className={styles.aboutBox}>
           <h2 className="title">Sobre mim</h2>
           <p>
-            Sou graduando em fase final do curso de{" "}
-            <span className="highlight">Sistemas de Informação</span> com
-            interesse em{" "}
+            Sou graduado em{" "}
+            <span className="highlight">Sistemas de Informação</span> pela{" "}
+            <span className="highlight">
+              Universidade Federal dos Vales do Jequitinhonha e Mucuri (UFVJM)
+            </span>{" "}
+            com interesse em{" "}
             <span className="highlight">
               desenvolvimento fullstack e DevOps.
             </span>{" "}
-            Tive contato com React, JavaScript, TypeScript, Python e Express,
-            além de ORMs para bancos de dados. Tenho habilidades em comunicação
+            Tenho experiência com React, JavaScript, TypeScript, Python e Express,
+            além de ORMs para bancos de dados. Também tenho noções sobre Docker e orquestração de containeres com Kubernetes. Possuo boas habilidades em comunicação
             interpessoal, trabalho em equipe e{" "}
             <span className="highlight">
               estou constantemente em busca de desafios!
