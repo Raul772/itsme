8150f1e4358c8b176361e8efeb7cb615f0e6fcc8
Raul Victor <31050006+Raul772@users.noreply.github.com>
2026-04-12 18:54:55 -0300

    feat: update color variables and add bouncing animation to images

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,12 +4,13 @@ import stars_grouped from "../../../assets/Stars-grouped.svg";
 import styles from "./About.module.css";
 
 export default function About() {
+
   return (
     <section id="about" className={styles.about}>
       <div className={`container ${styles.aboutContainer}`}>
         <div className={styles.aboutDecoration}>
-          <img src={box_eyes} alt="" />
-          <img src={stars_grouped} alt="" />
+          <img className={styles.bounce} src={box_eyes} alt="" />
+          <img className={styles.bounce} src={stars_grouped} alt="" />
         </div>
 
         <div className={styles.aboutBox}>
@@ -34,8 +35,8 @@ export default function About() {
         </div>
 
         <div className={styles.aboutDecoration}>
-          <img src={stars_grouped} alt="" />
-          <img src={box_circles} alt="" />
+          <img className={styles.bounce} src={stars_grouped} alt="" />
+          <img className={styles.bounce} src={box_circles} alt="" />
         </div>
       </div>
     </section>
